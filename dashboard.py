8190e58093b985f564fc8f206fd0de93acab0831
Teeejjj <teejayborromeo@gmail.com>
2025-06-07 10:36:53 +0800

    added dashboard-streamlit

---
+++ dashboard.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import streamlit as st
+
+import warnings
+import io
+import time
+
+warnings.filterwarnings('ignore')
+
+def main():
+    
+    st.title('2025 Budget Analysis 💸📊')
+
+
+if __name__ == '__main__':
+    main()
